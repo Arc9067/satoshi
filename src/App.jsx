7e678c24d7991d2120e6838c62d05c09a2b36af1
Mikail Rokib <atandarokib@gmail.com>
2023-04-28 13:16:29 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.scss";
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
 import About from "./Components/About/About";
+import Footer from "./Components/Footer/Footer";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Header />
       <Hero />
       <About />
+      <Footer />
     </>
   );
 }
